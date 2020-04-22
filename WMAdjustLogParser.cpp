f298dbe761bd7e077ef6e492f36dff1acf10db5a
Ghennadii <gghennadii@gmail.com>
2020-04-22 18:48:24 +0300

    updating the source files

--- WMAdjustLogParser.cpp
+++ WMAdjustLogParser.cpp
@@ -1,73 +1,225 @@
 #include "WMAdjustLogParser.hpp"
 
-WMAdjustLogParser::WMAdjustLogParser(string input, string output){
-	input_file = input;
-	output_file = output;
+//#define WMTempExcluded
+//#define SEP_CH                  "\t"
+#define SEP_CH                  ","
+
+NginxParser::NginxParser(string input_file, string output_file){
+	input.open(input_file);
+	output.open(output_file);
+
+    if (!output.is_open()){
+	    cerr << "Can't open the output file: " << output_file << endl;
+	    return;
+	}
+
+	if (!input.is_open()){
+	    output << "Can't open the input file! ";
+	    return;
+	}
+
+	for (uint8_t WM_idx = 0; WM_idx < MAX_COUNTERS_NR; WM_idx++){
+        check_WM_state |= WM_STATE(WM_idx);
+    }
 }
 
-void WMAdjustLogParser::parse(){
-	
-	ifstream input(input_file);
-	string line;
-	//char line[MAX_LINE];
-	int length=0;
-	
+using namespace std;
+
+void NginxParser::parse(){
+
+    string line;
+
 	while( std::getline(input, line) ){
-		
-		length = sizeof(line);
-		
-		
-		//if(strncmp(line+left+1,"CONNECT",7)==0 );
+        lines++;
+        float value = 0;
+        uint32_t WMID = 0;
 
-				
+        if (try_TBTemp(line, value)){
+            calibration_Point[points_idx.TB_temp_idx].TB_temp = value;
+            points_idx.TB_temp_idx++;
+            if (points_idx.TB_temp_idx == CALIB_ITER_NR){
+                collection_state.TB_temp_ok = 1;
+                points_idx.TB_temp_idx = 0;
+            }
 
-	
+            continue;
+        }
+
+        if (try_TBFlow(line, value)){
+            calibration_Point[points_idx.TB_flow_idx].TB_flow = value;
+            points_idx.TB_flow_idx++;
+            if (points_idx.TB_flow_idx == CALIB_ITER_NR){
+                collection_state.TB_flow_ok = 1;
+                points_idx.TB_flow_idx = 0;
+            }
+
+            continue;
+        }
+        if (try_WMTemp(line, value, WMID)){
+#ifndef WMTempExcluded
+            uint8_t WM_idx = locateWMinTable(WMID);
+
+            if (WM_TABLE_FULL != WM_idx){
+                calibration_Point[points_idx.WM_temp_idx[WM_idx]].counter_data[WM_idx].WM_temp = value;
+                points_idx.WM_temp_idx[WM_idx]++;
+                if (points_idx.WM_temp_idx[WM_idx] == CALIB_ITER_NR){
+                    collection_state.WM_temp_ok |= WM_STATE(WM_idx);
+                    points_idx.WM_temp_idx[WM_idx] = 0;
+                }
+            }
+#endif // WMTempExcluded
+            continue;
+        }
+
+        if (try_WMFlow(line, value, WMID)){
+            uint8_t WM_idx = locateWMinTable(WMID);
+
+            if (WM_TABLE_FULL != WM_idx){
+                calibration_Point[points_idx.WM_flow_idx[WM_idx]].counter_data[WM_idx].WM_flow = value;
+                points_idx.WM_flow_idx[WM_idx]++;
+                if (points_idx.WM_flow_idx[WM_idx] == CALIB_ITER_NR){
+                    collection_state.WM_flow_ok |= WM_STATE(WM_idx);
+                    points_idx.WM_flow_idx[WM_idx] = 0;
+                }
+            }
+            continue;
+        }
+
+        if (true == try_print_export()){
+            print_export();
+        }
+    }
+
+    if (true == try_print_export()){
+        print_export();
+    }
+
+    output << "Read " << lines <<" lines!\n";
+    cout << "Read " << lines <<" lines!\t" << "Valide: " << valid_lines << endl;
 	input.close();
 }
 
-	/*
-void WMAdjustLogParser::statistics(){
-	int current_max,w=35;
-	string ip,id;
-	
-	ofstream output(output_file);
-	output << setw(w) << left << "Number of unique IPs: " << users.size() << "\n\n";
-	output << setw(w) << "Number of unique contents: " << contents.size() << "\n\n";
-	output << setw(w) << "Total data sent: " << (double)total_data/1e9 << "GB\n\n";
-	output << setw(w) << "Number of video requests: " << videos << "\n\n";
-	output << setw(w) << "Number of audio requests: " << audios << "\n\n";
-	output << setw(w) << "Number of cs hits requests: " << cs_hits << "\n\n";
-	
-	current_max = 0;
-	for( auto &user: users )
-		if( user.second > current_max ){
-			current_max = user.second;
-			ip = user.first;
-		}
-	
-	output << setw(w) << "The IP that watched contents most: " << ip << " (" << current_max << " times)\n\n";
-	
-	current_max = 0;
-	for( auto &content: contents )
-		if( content.second > current_max ){
-			current_max = content.second;
-			id = content.first;
-		}
-	
-	output << setw(w) << "The content that watched most: " << id << " (" << current_max << " times)\n\n";
-	
-	output << "Browsers:\n";
-	for( auto &browser: user_agents )
-		output << browser.first << ": %" << ((double)browser.second/valid_lines)*100.0 << "\n";
-	
-	output << "\nHTTP Statuses:\n";
-	for( int i=0 ; i<5 ; i++ )
-		output << i+1 << "xx: %" << ((double)http_statuses[i]/valid_lines)*100.0 << "\n";
-	
-	output << "\nBitrates:\n";
-	for( auto &bitrate: bitrates )
-		output << bitrate.first << ": %" << ((double)bitrate.second/valid_lines)*100.0 << "\n";
-	
-	output.close();
+bool NginxParser::try_TBTemp(string &line, float &value){
+    bool line_corresponds = false;
+    string string2find = "Get TestBench Temperature:";
+    size_t pos = 0;
+
+    pos = line.find(string2find, 0);
+
+    if (string::npos != pos){
+        // Matching string Found!
+        line_corresponds = true;
+        value = atof((char*)&line[pos + string2find.length()]);
+    }
+
+    return line_corresponds;
+};
+
+bool NginxParser::try_TBFlow(string &line, float &value){
+    bool line_corresponds = false;
+    string string2find = "Get TestBench Flowrate:";
+    size_t pos = 0;
+
+    pos = line.find(string2find, 0);
+
+    if (string::npos != pos){
+        // Matching string Found!
+        line_corresponds = true;
+        value = atof((char*)&line[pos + string2find.length()]);
+    }
+
+    return line_corresponds;
+};
+
+bool NginxParser::try_WMTemp(string &line, float &value, uint32_t &WMID){
+    bool line_corresponds = false;
+    string string2find = "Get Saved Counter Temperature:";
+    string string2findCounter = "Counter ID:";
+    size_t pos1 = 0, pos2 = 0;
+
+    pos1 = line.find(string2find, 0);
+    pos2 = line.find(string2findCounter, 0);
+
+    if (string::npos != pos1 && string::npos != pos2){
+        // Matching string Found!
+        line_corresponds = true;
+        value = atof((char*)&line[pos1 + string2find.length()]);
+        WMID = atoi((char*)&line[pos2 + string2findCounter.length()]);
+    }
+
+    return line_corresponds;
+};
+
+bool NginxParser::try_WMFlow(string &line, float &value, uint32_t &WMID){
+    bool line_corresponds = false;
+    string string2find = "Get Saved Counter Flowrate:";
+    string string2findCounter = "Counter ID:";
+    size_t pos1 = 0, pos2 = 0;
+
+    pos1 = line.find(string2find, 0);
+    pos2 = line.find(string2findCounter, 0);
+
+    if (string::npos != pos1 && string::npos != pos2){
+        // Matching string Found!
+        line_corresponds = true;
+        value = atof((char*)&line[pos1 + string2find.length()]);
+        WMID = atoi((char*)&line[pos2 + string2findCounter.length()]);
+    }
+
+    return line_corresponds;
+};
+
+uint8_t NginxParser::locateWMinTable(const uint32_t WM_ID){
+    uint8_t WM_idx;
+    for (WM_idx = 0; WM_idx < MAX_COUNTERS_NR; WM_idx++){
+        if (WM_ID == WM_IDs[WM_idx]){
+            return WM_idx;
+        } else
+            if (0 == WM_IDs[WM_idx]){
+                WM_IDs[WM_idx] = WM_ID;
+                return WM_idx;
+        }
+    }
+    return WM_TABLE_FULL;
 }
-	*/
+
+bool NginxParser::try_print_export(){
+    bool export_log = false;
+
+    if (1 == collection_state.TB_flow_ok
+        && 1 == collection_state.TB_temp_ok)
+    {
+
+        for (uint8_t WM_idx = 0; WM_idx < MAX_COUNTERS_NR; WM_idx++){
+            if (WM_STATE(WM_idx) | collection_state.WM_flow_ok
+#ifndef WMTempExcluded
+                && WM_STATE(WM_idx) | collection_state.WM_temp_ok
+#endif // WMTempExcluded
+                && 0 != WM_IDs[WM_idx]){
+                   export_log = true;
+                   break;
+            }
+        }
+
+        collection_state.TB_flow_ok = 0;
+        collection_state.TB_temp_ok = 0;
+        collection_state.WM_flow_ok = 0;
+        collection_state.WM_temp_ok = 0;
+    }
+    return export_log;
+}
+
+void NginxParser::print_export(){
+    for (uint8_t idx = 0; idx < CALIB_ITER_NR; idx++){
+        valid_lines ++;
+        output  << valid_lines << SEP_CH
+                << calibration_Point[idx].TB_temp << SEP_CH
+                << calibration_Point[idx].TB_flow << SEP_CH;
+        for (uint8_t WM_idx = 0; WM_idx < MAX_COUNTERS_NR; WM_idx++){
+            output  << calibration_Point[idx].counter_data[WM_idx].WM_temp << SEP_CH
+                    << calibration_Point[idx].counter_data[WM_idx].WM_flow << SEP_CH
+                    << WM_IDs[WM_idx] << SEP_CH;
+        }
+        output  << "\n";
+    }
+}
--- WMAdjustLogParser.hpp
+++ WMAdjustLogParser.hpp
@@ -1,5 +1,5 @@
-#ifndef WMAdjustLogParser_HPP
-#define WMAdjustLogParser_HPP
+#ifndef NGINXPARSER_HPP
+#define NGINXPARSER_HPP
 
 #include <cstring>
 #include <iostream>
@@ -7,33 +7,72 @@
 #include <fstream>
 #include <unordered_map>
 
+#define WM_TABLE_FULL       0xFF
+
+#define STATE_OK    1
+#define STATE_NOK   0
+
+#define WM_STATE(WMIDX)    (1<<WMIDX)
+
+
 using namespace std;
 
-const int MAX_LINE = 500;
+const int CALIB_ITER_NR = 40;
+const int MAX_COUNTERS_NR = 10;
 
-typedef struct calib_measurement_t{
-	struct counter_data_t {
-		int WM_ID;
+typedef struct __attribute__((packed, aligned(4))) {
+    uint32_t TB_temp_ok:1;
+    uint32_t TB_flow_ok:1;
+    uint32_t WM_temp_ok:MAX_COUNTERS_NR;
+    uint32_t WM_flow_ok:MAX_COUNTERS_NR;
+    uint32_t reserved:((2+2*MAX_COUNTERS_NR)%4);
+} state_t;
+
+typedef struct calib_measurement{
+	typedef struct {
 		float WM_temp;
 		float WM_flow;
-	} ;
+	} counter_data_t;
+
 
 	float TB_temp;
 	float TB_flow;
-	counter_data_t counter_data[10];
-};
+	counter_data_t counter_data[MAX_COUNTERS_NR];
+} calib_measurement_t;
 
-class WMAdjustLogParser{
-	
+typedef struct {
+    uint8_t TB_temp_idx;
+    uint8_t TB_flow_idx;
+    uint8_t WM_temp_idx[MAX_COUNTERS_NR];
+    uint8_t WM_flow_idx[MAX_COUNTERS_NR];
+} points_idx_t;
+
+class NginxParser{
+
 	int lines=0, valid_lines=0, counters=0, TB_flows=0, TB_temp=0, counters_flows=0, counters_temp=0;
-	string input_file, output_file;
+	ifstream input;
+	ofstream output;
 
-	calib_measurement_t calibration_Point[40];
+	uint32_t check_WM_state = 0;
+	points_idx_t points_idx = {0};
+	uint32_t WM_IDs[MAX_COUNTERS_NR] = {0};
+	state_t collection_state = {0};
+	calib_measurement_t calibration_Point[CALIB_ITER_NR] = {0};
+
 	long long int total_data = 0;
-	
+
+    private:
+        bool try_TBTemp(string &line, float &value);
+        bool try_TBFlow(string &line, float &value);
+        bool try_WMTemp(string &line, float &value, uint32_t &WMID);
+        bool try_WMFlow(string &line, float &value, uint32_t &WMID);
+
+        uint8_t locateWMinTable(uint32_t WM_ID);
+        bool try_print_export();
+        void print_export();
+
 	public:
-		WMAdjustLogParser(string input, string output);
+		NginxParser(string input, string output);
 		void parse();
-		//void statistics();
 };
 #endif
